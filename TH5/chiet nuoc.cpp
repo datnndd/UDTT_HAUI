b27b39ffa025542f83ccb787b692eab921eedb02
Doan Dat <155744471+datnndd@users.noreply.github.com>
2024-06-13 14:36:38 +0700

    Update chiet nuoc.cpp

--- TH5/chiet nuoc.cpp
+++ TH5/chiet nuoc.cpp
@@ -1,54 +1,58 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
-void quick_sort(vector<int> &x, int left, int right){
-	if(left>=right)	return;
+void qsort(int x[], int left, int right) {
+	if (left >= right)	return;
 	int i = left, j = right;
 	int pivot = x[(left + right) / 2];
-	while(i<=j){
-		while(x[i] < pivot)	i++;
-		while(x[j] > pivot)	j--;
-		if(i<=j){
+	while (i <= j) {
+		while (x[i] < pivot) i++;
+		while (x[j] > pivot)	j--;
+		if (i <= j) {
 			swap(x[i], x[j]);
 			i++;
 			j--;
 		}
 	}
-	quick_sort(x, left, j);
-	quick_sort(x, i, right);
+	qsort(x, left, j);
+	qsort(x, i, right);
 }
 
-int A2(vector<int> x, int n, int d){
+int B2(int x[], int n, int d) {
 	int i = 0;
-	while(d>=x[i] && i<n){
-		d = d-x[i];
+	int ans = 0;
+	int m = d;
+	while (m > 0 && i < n) {
+		if (m >= x[i]) {
+			ans++;
+			m -= x[i];
+		}
 		i++;
 	}
-	if(i != 0) return i;
-	else return -1;
+	if (m == d)	return -1;
+	return ans;
 }
 
-void printBottles(vector<int> x, int n){
-	for (int i = 0; i<n; i++){
-		cout<<x[i]<<", ";
+void prt_arr(int x[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << x[i] << ' ';
 	}
-	cout<<endl;
+	cout << endl;
 }
 
-int main(){
-	int n = 6;
-	int d = 10;
-	vector<int> V_binh = {4, 5, 10, 1, 2, 3};
-	
-	quick_sort(V_binh, 0, n-1);
-	
-	int ans = A2(V_binh, n, d);
-	if(ans == -1)	cout<<"Khong co chai nao"<<endl;
-	else{
-		cout<<"So luong chai toi da duoc do day la: "<<ans<<endl;
-		cout<<"Dung tich cac chai lan luot la: ";
-		printBottles(V_binh, ans);
+int main() {
+	int n = 5;
+	int d = 1;
+	int v[] = {4, 5, 10, 2, 3};
+
+	qsort(v, 0, n - 1);
+
+	int ans = B2(v, n, d);
+	if (ans == -1)	cout << "Khong co chai nao" << endl;
+	else {
+		cout << "So luong chai toi da duoc do day la: " << ans << endl;
+		cout << "Dung tich cac chai lan luot la: ";
+		prt_arr(v, ans);
 	}
 	return 0;
 }
