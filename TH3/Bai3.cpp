e2c496655b4d8ba9f8cc143abb2703addbb3a428
Doan Dat <155744471+datnndd@users.noreply.github.com>
2024-06-26 17:25:11 +0700

    Update Bai3.cpp

--- TH3/Bai3.cpp
+++ TH3/Bai3.cpp
@@ -1,28 +1,20 @@
 #include<iostream>
 using namespace std;
 
-void sort_chiadetri(float arr[], int left, int right) {
-	if (left > right)	return;
-	int i = left, j = right;
-	float pivot = arr[(left + right) / 2];
-	while (i <= j) {
-		while (arr[i] < pivot)	i++;
-		while (arr[j] > pivot)	j--;
-		if (i <= j) {
-			swap(arr[i], arr[j]);
-			i++;
-			j--;
-		}
+float soMax(float arr[], int left, int right) {
+	if (left == right) {
+		return arr[left];
 	}
-	sort_chiadetri(arr, left, j);
-	sort_chiadetri(arr, i, right);
+	int m = (left + right) / 2;
+	float left_e = soMax(arr, left, m);
+	float right_e = soMax(arr, m+1, right);
+	if (left_e > right_e)	return left_e;
+	else return right_e;
 }
 
 int main() {
-	int n = 15;
-	float arr[] = { 1.1, 9, 8, 7, 5.5, 7.7, 4, 3 , 2.2, 9.9, 15.5, 44.4, 14.4, 14.9, 15.5 };
-	sort_chiadetri(arr, 0, 14);
-	for (int i = 0; i < n; i++)	cout << arr[i] << ' ';
-	cout << endl;
+	int n = 20;
+	float arr[] = { -1.1, 9, 8, 7, -5.5, -7.7, 4, 3 , -2.2, -9.9, -15.5, 6, -14.4, -14.9, -15.5, -5.5, 5, 10, 2, 1 };
+	cout << soMax(arr, 0, 19) << endl;
 	return 0;
 }
